f3578958a8e5e3e88cd675903676fcfbc898b855
YekaterinaFokina <bab.ashch.16@gmail.com>
2024-12-10 15:54:40 +0300

    commit1

---
+++ js/feedback.js
@@ -0,0 +1,7 @@
+
+    function OpenFeedbackWindow() {
+        document.getElementById("window_feedback").style.display = "block";
+}
+    function closeWindow_feedback() {
+        document.getElementById("window_feedback").style.display = "none";
+}
